8495796cf0fb15e7f251b978b3e54fe66b99e9e9
John Roquemore <john.roquemore@gmail.com>
2018-03-04 10:49:31 -0600

    Added the controller for adding a ToDo List item and it's class.

---
+++ To Do List/AddToDoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddToDoViewController.swift
+//  To Do List
+//
+//  Created by John Roquemore on 3/4/18.
+//  Copyright © 2018 John Roquemore. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+}
