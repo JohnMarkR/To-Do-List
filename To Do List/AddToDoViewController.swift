ef3619e161041cca10d8491339da683586b96b8e
John Roquemore <john.roquemore@gmail.com>
2018-03-04 15:22:01 -0600

    Worked on adding code to the Add Controller for the Add button.

--- To Do List/AddToDoViewController.swift
+++ To Do List/AddToDoViewController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+import CoreData
 
 class AddToDoViewController: UIViewController
 {
-
+    @IBOutlet weak var toDoItem: UITextField!
+    
+    var task = [ToDoList]()
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
     }
     
+    @IBAction func addTapped(_ sender: UIButton)
+    {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "ToDoList", in: context)
+        
+        let taskItem = ToDoList(entity: entity!, insertInto: context)
+        taskItem.toDoItem = toDoItem.text
+        
+        appDelegate.saveContext()
+        task.append(taskItem)
+        
+        //self.tableView.reloadData()
+        
+        
+    }
     // TODO: - Need Keyboard code to make it exit.
 
 }
